46da047b69c6ba1a67103d2c134dc377365577c0
Michal <doboszmichal4@gmail.com>
2023-07-30 00:35:37 +0200

    first commit

--- src/App.tsx
+++ src/App.tsx
@@ -11,12 +11,14 @@ function App() {
 
   return (
     <>
+      <HashRouter>
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/Destination" element={<Destination />} />
           <Route path="/Crew" element={<Crew />} />
           <Route path="/Technology" element={<Technology />} />
         </Routes>
+      </HashRouter>
     </>
   );
 }
