e7241f3ce76c5da1f93d904f17d0546b4fdd651b
Michal <doboszmichal4@gmail.com>
2023-07-29 23:15:57 +0200

    first commit

--- src/App.tsx
+++ src/App.tsx
@@ -6,35 +6,6 @@ import Crew from './Components/Subpages/Crew';
 import Technology from './Components/Subpages/Technology';
 
 function App() {
-
-  // function setHeaderBottomText(){
-  //   const headerBottomText = document.querySelector(".header--bottom-text")  as HTMLElement;
-  //   headerBottomText.style.visibility = 'visible'
-  //   headerBottomText.style.display = 'block'
-
-  //   const bodyBottomText = document.querySelector(".body--bottom-text ")  as HTMLElement;
-  //   bodyBottomText.style.visibility = 'visible'
-  //   bodyBottomText.style.display = 'block'
-  // }
-  // function showHeaderBottomText(){
-  //   setTimeout(()=> {setHeaderBottomText()},1500)
-  // }
-
-  // // function setBodyBottomText(){
-  // //   const bodyBottomText = document.querySelector(".body--bottom-text ")  as HTMLElement;
-  // //   bodyBottomText.style.visibility = 'visible'
-  // //   bodyBottomText.style.display = 'block'
-  // // }
-  // // function showBodyBottomText(){
-  // //   setTimeout(()=> {setBodyBottomText()},3000)
-  // // }
-
-  // useEffect(() => {
-  //   showHeaderBottomText()
-  //   // showBodyBottomText()
-  // },[])
-  
-
   
 
   return (
